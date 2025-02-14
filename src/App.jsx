04b1684eb24dc02edfbbd6ca591e9f0298076c8d
dariomedina10 <dariomedina10@gmail.com>
2025-02-14 11:40:26 -0400

    Imagenes e iniciode hoja d estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import logo from './imagenes/freecodecamp.png';
 import './App.css'
 
 function App() {
@@ -8,11 +7,13 @@ function App() {
 
   return (
     <>
-      <div>
-      
+      <div className='App'>
+      <div className='freecodecamp-logo-contenedor'>
+        <img src={logo} className='freecodecamp-logo' alt='logo' />
+        </div>   
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Darío Vite
       </p>
     </>
   )
