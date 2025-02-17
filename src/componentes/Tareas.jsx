9981f7bad9136bbcee3dcf8b2f31293e9a8ac475
dariomedina10 <dariomedina10@gmail.com>
2025-02-17 12:44:05 -0400

    Armando Formato de Lista de Tareas II

---
+++ src/componentes/Tareas.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+Function Tarea ( {texto}){
+
+    return (
+      <div className='tarea-contenedor'>
+        <div className='tarea-texto'>
+          {texto}
+        </div>
+        <div className='tarea-icono'   >
+          Eliminar
+        </div>
+
+      </div>
+
+    );
+}
+
+export defaull tarea;
