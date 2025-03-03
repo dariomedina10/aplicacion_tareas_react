fe3088e64c673d0d24d128c915124564b5ecd436
dariomedina10 <dariomedina10@gmail.com>
2025-03-03 03:05:32 -0400

    Creacion de Componente y Hoja de EStilo Lista de Tareas

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,51 @@
-import { useState } from 'react'
+import './App.css';
 import logo from './imagenes/freecodecamp.png';
-import './App.css'
-import Tarea from './componentes/Tareas.jsx';
-import TareaFormulario from './componentes/TareaFormulario.jsx';
+import ListaDeTareas from './componentes/ListaDeTareas';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div className='App'>
+    <div className='aplicacion-tareas'>
       <div className='freecodecamp-logo-contenedor'>
-        <img src={logo} className='freecodecamp-logo' alt='logo' />
-        </div> 
-        <div className='tareas-lista-principal'>
-          <h1>Mis Tareas</h1>
-          <TareaFormulario />
-          <Tarea texto='Aprender React' />
-          </div>  
+      <img src={logo} className='freecodecamp-logo' alt='logo' />
       </div>
-      <p className="read-the-docs">
-       
-      </p>
-    </>
-  )
+      <div className='tareas-lista-principal'>
+        <h1>Mis Tareas</h1>
+        <ListaDeTareas />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
+
+
+// import { useState } from 'react'
+// import logo from './imagenes/freecodecamp.png';
+// import './App.css'
+// import Tarea from './componentes/Tareas.jsx';
+// import TareaFormulario from './componentes/TareaFormulario.jsx';
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div className='App'>
+//       <div className='freecodecamp-logo-contenedor'>
+//         <img src={logo} className='freecodecamp-logo' alt='logo' />
+//         </div> 
+//         <div className='tareas-lista-principal'>
+//           <h1>Mis Tareas</h1>
+//           <TareaFormulario />
+//           <Tarea texto='Aprender React' />
+//           </div>  
+//       </div>
+//       <p className="read-the-docs">
+       
+//       </p>
+//     </>
+//   )
+// }
+
+// export default App
--- src/componentes/Tareas.jsx
+++ src/componentes/Tareas.jsx
@@ -1,22 +1,45 @@
 import React from 'react';
-import '../hoja-de-estilos/tareas.css'
-import { AiOutlineCloseCircle } from 'react-icons/ai';
+import '../hoja-de-estilos/Tareas.css';
+import { AiOutlineCloseCircle } from "react-icons/ai";
 
-function Tarea ( {texto,completada}){
-
-    return (
-      <div className= {completada  ? 'tarea-contenedor completada' : 'tarea-contenedor' }> 
-        <div className='tarea-texto' >
-          {texto}
-        </div>
-        <div className='tarea-contenedor-iconos'   >
-       < AiOutlineCloseCircle className='tarea-icono'/>
-        </div>
-
+function Tarea({ id, texto, completada, completarTarea, eliminarTarea }) {
+  return (
+    <div className={completada ? 'tarea-contenedor completada' : 'tarea-contenedor'}>
+      <div 
+        className='tarea-texto'
+        onClick={() => completarTarea(id)}>
+        {texto}
       </div>
-
-    );
+      <div 
+        className='tarea-contenedor-iconos'
+        onClick={() => eliminarTarea(id)}>
+        <AiOutlineCloseCircle className='tarea-icono' />
+      </div>
+    </div>
+  );    
 }
 
 export default Tarea;
 
+
+// import React from 'react';
+// import '../hoja-de-estilos/tareas.css'
+// import { AiOutlineCloseCircle } from 'react-icons/ai';
+
+// function Tarea ( {texto,completada}){
+
+//     return (
+//       <div className= {completada  ? 'tarea-contenedor completada' : 'tarea-contenedor' }> 
+//         <div className='tarea-texto' >
+//           {texto}
+//         </div>
+//         <div className='tarea-contenedor-iconos'   >
+//        < AiOutlineCloseCircle className='tarea-icono'/>
+//         </div>
+
+//       </div>
+
+//     );
+// }
+
+// export default Tarea;
